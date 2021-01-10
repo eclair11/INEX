caaaef6b38b4b2ec6189471a56e84f3e9400c8f5
Nicolas <nicolas.trotta@etu.univ-st-etienne.fr>
2021-01-10 17:28:00 +0100

    Added methods for retrieving <link> tags from file contents and processing their information.

---
+++ src/main/java/org/inex/Model/GraphLink.java
@@ -0,0 +1,164 @@
+package org.inex.Model;
+
+import java.util.ArrayList;
+
+import org.inex.Utils.UtilArticleLink;
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultWeightedEdge;
+
+    /****************/
+    /** GraphLink  **/
+    /****************/
+
+public class GraphLink {
+
+    // Variables
+    private Graph<String, DefaultWeightedEdge> multiGraph;
+
+    private int totalNbVertex; // Number of articles / Number of referenced xml files
+
+    private int totalNbIncomingLink; // In-degree / Liens entrants
+
+    private int totalNbOutgoingLink; // Out-degree / Liens sortants
+
+    private ArrayList<ArticleVertex> articleVertexList;
+
+    // Constructors
+
+    public GraphLink(){
+
+    }
+
+    public GraphLink(ArrayList<Doc> docList) {
+
+        // Build the list of all xml files
+        ArrayList<String> articleList = UtilArticleLink.createArticleList(docList);
+
+        // Create, init the graph with each article as a vertex and compute the size of the graph 
+        this.multiGraph = UtilArticleLink.createGraph();
+        this.multiGraph = UtilArticleLink.fillGraphVertex(this.multiGraph, articleList);
+        this.totalNbVertex = this.multiGraph.vertexSet().size();
+        this.totalNbIncomingLink = 0;
+        this.totalNbOutgoingLink = 0;
+
+        // Init each ArticleVertex of the graph
+        this.articleVertexList = new ArrayList<>();
+        docList.forEach(doc -> articleVertexList.add(new ArticleVertex(doc.getId())));
+
+        // We build the list of linked article lists
+        docList.forEach(doc -> {
+            ArrayList<String> linkedArticleList = new ArrayList<>();
+            linkedArticleList =  UtilArticleLink.getAllLinkedArticle(doc.getContent());
+            this.multiGraph = UtilArticleLink.fillArticleGraphEdge(this.multiGraph, doc.getId(), linkedArticleList);
+        });
+        
+        UtilArticleLink.printGraph(this.multiGraph);
+
+    }
+
+    // Getters && Setters
+    public Graph<String, DefaultWeightedEdge> getMultiGraph() {
+        return multiGraph;
+    }
+
+    public void setMultiGraph(Graph<String, DefaultWeightedEdge> multiGraph) {
+        this.multiGraph = multiGraph;
+    }
+
+    public int getTotalNbVertex() {
+        return totalNbVertex;
+    }
+
+    public void setTotalNbVertex(int totalNbVertex) {
+        this.totalNbVertex = totalNbVertex;
+    }
+
+    public int getTotalNbIncomingLink() {
+        return totalNbIncomingLink;
+    }
+
+    public void setTotalNbIncomingLink(int totalNbIncomingLink) {
+        this.totalNbIncomingLink = totalNbIncomingLink;
+    }
+
+    public int getTotalNbOutgoingLink() {
+        return totalNbOutgoingLink;
+    }
+
+    public void setTotalNbOutgoingLink(int totalNbOutgoingLink) {
+        this.totalNbOutgoingLink = totalNbOutgoingLink;
+    }
+
+    
+    public ArrayList<ArticleVertex> getArticleVertexList() {
+        return articleVertexList;
+    }
+
+    public void setArticleVertexList(ArrayList<ArticleVertex> articleVertexList) {
+        this.articleVertexList = articleVertexList;
+    }
+
+
+    /********************/
+    /** ArticleVertex  **/
+    /********************/
+
+    public class ArticleVertex{
+
+        // Variables
+        private String id;
+
+        private int incomingLink; // In-degrees / Liens entrants
+
+        private int outgoingLink; // Out-degrees / Liens sortants
+
+        private int popularity; // Need to be computed
+
+        // Constructors
+        public ArticleVertex() {
+            
+        }
+
+        public ArticleVertex(String id) {
+            this.id = id;
+            this.incomingLink = 0;
+            this.outgoingLink = 0;
+            this.popularity = 0;
+        }
+
+        // Getters && Setters
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public int getIncomingLink() {
+            return incomingLink;
+        }
+
+        public void setIncomingLink(int incomingLink) {
+            this.incomingLink = incomingLink;
+        }
+
+        public int getOutgoingLink() {
+            return outgoingLink;
+        }
+
+        public void setOutgoingLink(int outgoingLink) {
+            this.outgoingLink = outgoingLink;
+        }
+
+        public int getPopularity() {
+            return popularity;
+        }
+
+        public void setPopularity(int popularity) {
+            this.popularity = popularity;
+        }
+
+    }
+
+}
---
+++ src/main/java/org/inex/Utils/UtilArticleLink.java
@@ -0,0 +1,134 @@
+package org.inex.Utils;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.inex.Model.Doc;
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.DirectedWeightedPseudograph;
+import org.jgrapht.traverse.DepthFirstIterator;
+
+public class UtilArticleLink {
+
+    /*************/
+    /** Methods **/
+    /*************/
+
+    public static Graph<String, DefaultWeightedEdge> createGraph() {
+        Graph<String, DefaultWeightedEdge> multiGraph = new DirectedWeightedPseudograph<>(DefaultWeightedEdge.class);
+        return multiGraph;
+    }
+
+    /** Just some tests  */
+    public static void testGraph(Graph<String, DefaultWeightedEdge> multiGraph){
+        multiGraph.addVertex("v1");
+        multiGraph.addVertex("v2");
+        multiGraph.addVertex("v3");
+        multiGraph.addVertex("v4");
+
+        multiGraph.addEdge("v1", "v2");
+        multiGraph.addEdge("v1", "v3");
+        multiGraph.addEdge("v1", "v4");
+        multiGraph.addEdge("v1", "v1");
+        multiGraph.addEdge("v4", "v1");
+        multiGraph.addEdge("v1", "v1");
+
+        multiGraph.getAllEdges("v1", "v4");
+        multiGraph.containsEdge("v1", "v4");
+        multiGraph.vertexSet();
+        System.out.println(multiGraph.containsEdge("v1", "v2"));
+        System.out.println("Degres entrants de V1 : " + multiGraph.inDegreeOf("v1"));
+        System.out.println("Degres sortants de V1 : " + multiGraph.outDegreeOf("v1"));
+        System.out.println("Liste des degres sortants de V1 : " + multiGraph.outgoingEdgesOf("v1"));
+    }
+
+    /** Receive the list of the articles used in the run and build all the vertexs of the multigraph. 
+     *  @return Graph<String, DefaultWeightedEdge> multiGraph
+     */
+    public static Graph<String, DefaultWeightedEdge> fillGraphVertex(Graph<String, DefaultWeightedEdge> multiGraph, ArrayList<String> articleList) {
+        articleList.forEach(article -> multiGraph.addVertex(article));
+        return multiGraph;
+    }
+
+    /** Receive the String content of an article used in the run and build all the edges from the link of the other articles. 
+     *  @return Graph<String, DefaultWeightedEdge> multiGraph
+     */
+    public static Graph<String, DefaultWeightedEdge> fillArticleGraphEdge(Graph<String, DefaultWeightedEdge> multiGraph, String currentArticle, ArrayList<String> linkedArticleList) {
+        linkedArticleList.forEach(linkedArticle -> {
+            if(multiGraph.containsVertex(linkedArticle)){
+                multiGraph.addEdge(currentArticle, linkedArticle);
+            }
+        });
+        return multiGraph;
+    }
+
+    public static void printGraph(Graph<String, DefaultWeightedEdge> multiGraph) {
+        System.out.println("\nCartographie du graphe des liens entre les articles :");
+        Iterator<String> iter = new DepthFirstIterator<>(multiGraph);
+        while (iter.hasNext()) {
+            String vertex = iter.next();
+            if(multiGraph.edgesOf(vertex) != null){
+                System.out.println("[" + vertex + " ] => " + multiGraph.edgesOf(vertex).toString());
+            }
+            
+        }
+    }
+
+    /** 
+     * Extract all of the linked articles from the content of a given article
+     * 
+     * @return ArrayList<String> linkedArticleList
+     */
+    public static ArrayList<String> getAllLinkedArticle(String article) {
+        ArrayList<String> linkedArticleList = new ArrayList<>();
+
+        // Regex to extract only the file id of the article from the link pointing to it
+        String regex = "(?<=\\/)([0-9]*)(?=\\.xml)";
+        Pattern pattern = Pattern.compile(regex);
+
+        Matcher matcher = pattern.matcher(article);
+
+        while (matcher.find()) {
+            linkedArticleList.add(matcher.group());
+        }
+
+        System.out.println("\nListe des articles pointés par un lien dans l'article:");
+        linkedArticleList.forEach(linkedArticle -> System.out.println(linkedArticle));
+
+        return linkedArticleList;
+    }
+
+    /**
+     * 
+     * @param docList
+     * @return ArrayList<String> articleList
+     */
+    public static ArrayList<String> createArticleList(ArrayList<Doc> docList){
+        ArrayList<String> articleList = new ArrayList<>();
+        docList.forEach(doc -> articleList.add(doc.getId()));
+        return articleList;
+    }
+
+
+    /**********/
+    /** Main **/
+    /**********/
+
+    public static void main(String[] args) {
+        // Creation du graph
+        Graph<String, DefaultWeightedEdge> multiGraph = createGraph();
+        testGraph(multiGraph);
+        printGraph(multiGraph);
+
+        // Test de récupération de l'id des articles dans une chaine
+        String test = "<link xlink:type=\"simple\" xlink:href=\"../442/14986442.xml\">"
+        + "<link xlink:type=\"simple\" xlink:href=\"../827/18753827.xml\">"
+        + "<link xlink:type=\"simple\" xlink:href=\"../631/226631.xml\">";
+        getAllLinkedArticle(test);
+
+    }
+    
+}
